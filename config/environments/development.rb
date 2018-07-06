940bb82fdf1709347c08fcd413b90234eb9249d7
alexbispo <alexbispo.artes@gmail.com>
2018-07-06 02:21:14 +0000

    Criação do modelo Category.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,4 +45,7 @@ Rails.application.configure do
   # Devise required
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # Foreman fix
+  $stdout.sync = true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "criando categorias..."
+categories = ["Animais e acessórios",
+							"Esportes",
+							"Para a sua, casa",
+							"Eletrônicos e celulares",
+							"Música e hobbies",
+							"Bebês e crianças",
+							"Moda e beleza",
+							"Veículos e barcos",
+							"Imóveis",
+							"Empregos e negócios"]
+
+categories.each { |c| Category.find_or_create_by(description: c) }
+puts "categorias criadas com sucesso!"
